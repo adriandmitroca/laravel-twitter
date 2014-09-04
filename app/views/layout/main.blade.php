1780a952749abe50e4ccea71eb8cfb3810396537
Adrian Dmitroca <dmitroca.adrian@gmail.com>
2014-09-04 11:10:46 +0200

    basic layout

--- app/views/layout/main.blade.php
+++ app/views/layout/main.blade.php
@@ -1,9 +1,58 @@
 <!DOCTYPE html>
-<html>
-<head>
-	<title></title>
-</head>
-<body>
-@yield('content')
-</body>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Twitter</title>
+
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+
+  	<div class="container-fluid">
+  		<div class="col-md-12">
+  			<nav class="navbar navbar-default navbar-fixed-top" role="navigation">
+				  <div class="container-fluid">
+				    <div class="navbar-header">
+				      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+				        <span class="sr-only">Toggle navigation</span>
+				        <span class="icon-bar"></span>
+				        <span class="icon-bar"></span>
+				        <span class="icon-bar"></span>
+				      </button>
+				      <a class="navbar-brand" href="#">Twitter</a>
+				    </div>
+
+				    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+				      <ul class="nav navbar-nav">
+				        <li class="active"><a href="#">Home</a></li>
+				        <li><a href="#">Timeline</a></li>
+				        <li><a href="#">Profile</a></li>
+				        <li><a href="#">Trends</a></li>
+				      </ul>
+
+				      <ul class="nav navbar-nav navbar-right">
+				        <li><a href="#">Log in</a></li>
+				        <li><a href="#">Sign up</a></li>
+				      </ul>
+				    </div>
+				  </div>
+				</nav>
+		</div>
+
+		<div class="content">
+			@yield('content')
+		</div>
+    </div>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+  </body>
 </html>
